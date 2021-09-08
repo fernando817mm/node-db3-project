df8426004005eeae4e58a139310ec1ba659f63e8
Fernando Martinez <fernando817mm@gmail.com>
2021-09-08 17:20:54 -0500

    cleaning up code

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -60,13 +60,6 @@ function add(scheme) {
 }
 
 function addStep(scheme_id, step) {
-  // EXERCISE E
-  /*
-    1E- This function adds a step to the scheme with the given `scheme_id`
-    and resolves to _all the steps_ belonging to the given `scheme_id`,
-    including the newly created one.
-  */
-
   return db("steps")
     .insert({
       ...step,
